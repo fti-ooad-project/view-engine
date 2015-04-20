e7affb194dae082cfb5e16a03b055ab43280a8f7
prostomen <antonshreyner@gmail.com>
2015-04-20 23:44:11 +0600

    попро

--- view/3dgl/RLight.h
+++
@@ -1,4 +0,0 @@
-#ifndef __RLIGHT_H__
-#define __RLIGHT_H__
-#include "RPassDesc.h"
-#endif /* __RLIGHT_H__ */
--- view/GL.h
+++
@@ -1,7 +0,0 @@
-#ifndef GL_H
-#define GL_H
-#include <GL/glew.h>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_opengl.h>
-void assertGL(int);
-#endif // GL_H
